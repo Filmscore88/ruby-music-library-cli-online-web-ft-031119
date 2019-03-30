049b6b3125d539f9cdb82e57dc05397616d17eee
Alberto Morales <cmtmorales24@gmail.com>
2019-03-30 16:09:48 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -7,8 +7,8 @@ class MusicLibraryController
   end 
   
   def call 
-   user_input=gets.chomp
-   unless user_input == "exit"
+    user_input= gets.chomp
+    while user_input != "exit"
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
