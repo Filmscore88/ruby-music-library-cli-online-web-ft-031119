82ecf444d70a409136a4cda7c259856802d7ecad
Alberto Morales <cmtmorales24@gmail.com>
2019-03-30 16:08:08 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -18,9 +18,7 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-      if user_input == "list songs"
-        self.list_songs 
-      end 
+      
    end 
 end 
 
